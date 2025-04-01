455a091d2351c03aa55673adcbc7b167403532e2
Chowdri Sakthivel <114232036+ChowdriS@users.noreply.github.com>
2025-04-01 11:55:50 +0530

    task4 completed

---
+++ Task4/script.js
@@ -0,0 +1,54 @@
+const apiKey = '82aa14ef4e829c8d057118249a51b4c0';
+const weatherInfo = document.getElementById('weatherInfo');
+const errorMessage = document.getElementById('errorMessage');
+const cityInput = document.getElementById('cityInput');
+
+async function getWeather() {
+    const city = cityInput.value.trim();
+    
+    if (!city) {
+        showError('Please enter a city name');
+        return;
+    }
+
+    try {
+        const response = await fetch(
+            `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}`
+        );
+        // console.log(response)
+        if (!response.ok) {
+            throw new Error('City not found');
+        }
+
+        const data = await response.json();
+        console.log(data)
+        displayWeather(data);
+    } catch (error) {
+        showError(error.message);
+    }
+}
+
+function displayWeather(data) {
+    const temperature = document.getElementById('temperature');
+    const description = document.getElementById('description');
+    const humidity = document.getElementById('humidity');
+
+    temperature.textContent = `${Math.round(data.main.temp - 273.15)}°C`;
+    description.textContent = data.weather[0].description;
+    humidity.textContent = `${data.main.humidity}%`;
+
+    errorMessage.classList.remove('active');
+    weatherInfo.classList.add('active');
+}
+
+function showError(message) {
+    errorMessage.textContent = message;
+    errorMessage.classList.add('active');
+    weatherInfo.classList.remove('active');
+}
+
+cityInput.addEventListener('keypress', function(e) {
+    if (e.key === 'Enter') {
+        getWeather();
+    }
+});
